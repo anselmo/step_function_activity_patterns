d584d899b8a3f22d2dc8bf0636cccb5b48fbb1f8
anselmo <arodriguesdasilva@expedia.com>
2018-03-14 17:10:26 +0000

    add template scheduler

--- athena_query_pull_mode/query.sh
+++ athena_query_pull_mode/query.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-input='{"query":"SELECT * FROM \"bookings\".\"dbsample\" limit 10;","outputLocation":"s3://aws-"}'
+input='{"query":"SELECT * FROM \"bookings\".\"dbsample\" limit 10;","outputLocation":"s3://ards-us-west-athena-results"}'
 aws stepfunctions start-execution --state-machine-arn <state-machine-arn> --input "$input"
